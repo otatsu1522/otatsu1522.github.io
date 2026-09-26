5cadc0cf38ccf5a4f635cd9cf60ee3b6722223ef
otatsu1522 <otatsu1522@gmail.com>
2026-09-26 16:49:50 +0900

    update

--- src/data/ui.ts
+++ src/data/ui.ts
@@ -46,7 +46,7 @@ export const ui = {
       noPosts: '新着情報はありません。',
     },
     footer: {
-      rights: 'All rights reserved.',
+      copyRights: 'OTATSU All rights reserved.',
       scrollTop: 'SCROLL TOP',
     },
   },
@@ -97,7 +97,7 @@ export const ui = {
       noPosts: 'No posts available yet.',
     },
     footer: {
-      rights: 'All rights reserved.',
+      rights: 'OTATSU All rights reserved.',
       scrollTop: 'SCROLL TOP',
     },
   },
